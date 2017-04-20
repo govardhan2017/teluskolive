d64c9882dbed97dd447afe6de80c1fa1bfaeb720
navinreddy20 <navinreddy20@gmail.com>
2017-04-20 17:06:29 +0530

    Add files via upload

---
+++ FeedbackDao.java
@@ -0,0 +1,14 @@
+package com.telusko.tusk.dao;
+
+import org.springframework.stereotype.Component;
+
+import com.telusko.tusk.entity.Feedback;
+
+@Component
+public class FeedbackDao 
+{
+	public void addFeedback(Feedback fb)
+	{
+		System.out.println("in addFeedback " + fb);
+	}
+}
